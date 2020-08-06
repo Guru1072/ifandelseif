46b3805023864a1ddfedec0363091082b15d3f62
Guru1072 <guru4goal@outlook.com>
2020-08-06 16:52:37 +0530

    Initial commit

---
+++ ifnifelse.js
@@ -0,0 +1,19 @@
+var myAge = 9;
+
+if (myAge > 30) {
+    document.write("you are over 30!");
+    } else if(myAge > 20)
+    
+{
+    document.write("you are over 20!");
+    
+}else if(myAge > 10)
+    
+{
+    document.write("youa are over 10!");
+    
+}
+else
+{
+    document.write("You are a kid!!!");
+}
